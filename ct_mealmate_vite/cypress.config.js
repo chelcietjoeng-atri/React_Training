ba89efcd74997466be35b6cf8a5bbee93117fd80
Chelcie Tjoeng <tjoeng@atrihub.io>
2025-05-07 14:33:42 -0700

    test(training): React Demo
    Fix .config files for Cypress testing [ICTRAINING-2106]

--- ct_mealmate_vite/cypress.config.js
+++ ct_mealmate_vite/cypress.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "cypress";
+// cypress.config.js
+
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:5173',
   },
 });
